a3c8b910bc878ddf46ad96ae065d8b4e160dcca5
David Hardzinski <dmhardzi@mtu.edu>
2025-05-14 23:26:13 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Wombat on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
